52ec490931a2721ba50b2c9678e27b3e45b15c47
sirprize <hoegl@backdoor.ch>
2012-11-26 21:18:32 +0100

    set initial value and sync only when val has changed

--- sync.js
+++ sync.js
@@ -1,15 +1,21 @@
 define([], function () {
     "use strict";
-    
+
     return function (source, sourceProp, target, targetProp) {
-        var s2t = source.watch(sourceProp, function (p, old, val) {
-            target.set(targetProp, val);
+        var s2t = source.watch(sourceProp, function (prop, old, val) {
+            if (target.get(targetProp) !== val) {
+                target.set(targetProp, val);
+            }
         });
-        
-        var t2s = target.watch(targetProp, function (p, old, val) {
-            source.set(sourceProp, val);
+
+        var t2s = target.watch(targetProp, function (prop, old, val) {
+            if (source.get(sourceProp) !== val) {
+                source.set(sourceProp, val);
+            }
         });
-        
+
+        target.set(targetProp, source.get(sourceProp));
+
         return {
             remove: function () {
                 s2t.remove();
